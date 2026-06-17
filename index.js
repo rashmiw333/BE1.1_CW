def06bf6463aa7ac04152342800be56d93b68131
rashmiw333 <rashmiwankhade99@gmail.com>
2026-06-17 14:52:23 +0530

    http methods to communicate with mongo db

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const express = require("express");
+const app = express();
 const {initializeDatabase} = require("./db/db.connect");
  const Movie = require("./models/movie.models");
+
+ app.use(express.json());
 
 initializeDatabase();
 
@@ -36,25 +40,49 @@ initializeDatabase();
  async function readMovieByTitle(movieTitle){
     try{
         const movie = await Movie.find({title: movieTitle});
-        console.log(movie);
+        return movie;
     }catch(error){
         throw error;
     }
 
  }
-//  readMovieByTitle("Lagaan")
+
+ app.get("/movies/:title",async(req,res)=>{
+    try{
+        const movie = await readMovieByTitle(req.params.title)
+        if(movie){
+        res.json(movie);
+        }else{
+            res.status(404).json({error:'Movie not Found'})
+        }
+    }catch(error){
+        res.status(500).json({error:"Failed to fetch movie"});
+    }
+ })
 
  //find all movies
 
   async function readAllMovies(){
     try{
         const allMovies = await Movie.find();
-        console.log(allMovies);
+        return allMovies;
     }catch(error){
         throw error;
     }
-
  }
+
+ app.get("/movies",async(req,res)=>{
+    try{
+        const movies = await readAllMovies()
+        if(movies.length !=0){
+            res.json(movies);
+        }else{
+            res.status(404).json({error:"No movies found"})
+        }
+    }catch(error){
+        res.status(500).json({error: "Failed to fetch Movie."})
+    }
+ })
 
 //  readAllMovies();
 
@@ -63,10 +91,48 @@ initializeDatabase();
  async function readByDirector(directorName){
     try{
         const movieByDirector = await Movie.find({director: directorName});
-        console.log(movieByDirector);
+        return movieByDirector;
     }catch(error){
         throw error;
     }
  }
 
- readByDirector("Rajkumar Hirani");
+app.get("/movies/director/:director",async(req,res)=>{
+    try{
+        const movies = await readByDirector(req.params.director)
+        if(movies.length !=0){
+            res.json(movies)
+        }else{
+            res.status(404).json({error:"Movie not Found"})
+        }
+
+    }catch(error){
+        res.status(500).json({error:"Failed to fetch Movie"})
+    }
+})
+
+async function readMovieByGenre(genreName){
+    try{
+        const movieGenre = await Movie.find({genre:genreName});
+        return movieGenre;
+    }catch(error){
+        throw error;
+    }
+}
+
+app.get("/movies/genre/:genreName",async(req,res)=>{
+    try{
+        const movies = await readMovieByGenre(req.params.genreName);
+        if(movies.length !=0){
+            res.json(movies);
+        }else{
+            res.status(404).json({error:"No movie found"});
+        }
+    }catch(error){
+        res.status(500).json({error:"failed to fetch movies"});
+    }
+})
+ const PORT = 3000
+ app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+ })
